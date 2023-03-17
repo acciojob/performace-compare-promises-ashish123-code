4a8c2732a16042398b38112ec2d0e5a3ecb04a4f
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-03-17 20:36:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,23 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+const startTimeAll = Date.now();
+Promise.all(apiUrls.map(url => fetch(url)))
+  .then(responses => {
+    const endTimeAll = Date.now();
+    const timeTakenAll = endTimeAll - startTimeAll;
+    document.getElementById("output-all").innerHTML = timeTakenAll;
+    console.log(responses);
+  })
+  .catch(error => console.log(error));
 
+const startTimeAny = Date.now();
+Promise.any(apiUrls.map(url => fetch(url)))
+  .then(response => {
+    const endTimeAny = Date.now();
+    const timeTakenAny = endTimeAny - startTimeAny;
+    document.getElementById("output-any").innerHTML = timeTakenAny;
+    console.log(response);
+  })
+  .catch(error => console.log(error));
 // You can write your code here
